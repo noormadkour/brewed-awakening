e2c95c95d2a2609d8f67d72b2c90047f54a99740
Noor Madkour <noormadkour@gmail.com>
2023-07-28 22:52:09 -0500

    removed debugger command

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -8,43 +8,34 @@ const orders = getOrders();
 // Function whose responsibility is to find the product for an order
 const findProduct = (order, products) => {
   let orderProduct = null;
-
   for (const product of products) {
     if (product.id === order.productId) {
       orderProduct = product;
     }
   }
-
   return orderProduct;
 };
 
 // Function whose responsibility is to find the employee for an order
 const findEmployee = (order, employees) => {
   let orderEmployee = null;
-
   for (const employee of employees) {
     if (employee.id === order.employeeId) {
       orderEmployee = employee;
     }
   }
-
   return orderEmployee;
 };
 
 export const Orders = () => {
   let html = "<ul>";
-
   for (const order of orders) {
     let employee = findEmployee(order, employees);
     let product = findProduct(order, products);
-
     html += `<li>${product.name} was sold by ${employee.name} on ${new Date(
       order.timestamp
     ).toLocaleDateString()}</li>`;
   }
-
   html += "</ul>";
-
   return html;
-  debugger;
 };
